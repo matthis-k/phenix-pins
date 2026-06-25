c609af35a6ce71aaec2e6a0cb9e8601b9dec10db
matthis-k <matthis.kaelble@gmail.com>
2026-06-25 14:24:22 +0200

    refactor: use flake-parts with module tree

--- flake.nix
+++ flake.nix
@@ -2,12 +2,14 @@
   description = "Phenix shared nixpkgs and tooling pins";
 
   inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
-  outputs = inputs: {
-    nixpkgs = inputs.nixpkgs;
-    nixpkgs-stable = inputs.nixpkgs-stable;
-  };
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      imports = [ ./modules ];
+    };
 }
