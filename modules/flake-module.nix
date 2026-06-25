4999ff284264fea7880502084ab86c54716fce75
matthis-k <matthis.kaelble@gmail.com>
2026-06-26 01:03:50 +0200

    chore: remove hello overlays from pins
    Change-Set: 091f99c2
    Workspace: phenix
    Managed-By: stitch

--- modules/flake-module.nix
+++ modules/flake-module.nix
@@ -1,11 +1,4 @@
 { ... }: {
   perSystem = { ... }: {
-    phenix.overlays = [(final: prev: {
-      phenix = (prev.phenix or {}) // {
-        hello-pins = final.writeShellScriptBin "hello-pins" ''
-          echo "hello from pins"
-        '';
-      };
-    })];
   };
 }
