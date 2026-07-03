b3e29c729eb3d1650dd2232295950ab86a4fa903
matthis-k <matthis.kaelble@gmail.com>
2026-07-03 07:09:30 +0200

    phenix-pins: add home-manager and sops-nix provider pins
    Chunk 1: API groundwork
    
    - Add home-manager input (follows nixpkgs)
    - Add sops-nix input (follows nixpkgs)
    - Update flake.lock with new provider entries

--- flake.nix
+++ flake.nix
@@ -8,6 +8,16 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
